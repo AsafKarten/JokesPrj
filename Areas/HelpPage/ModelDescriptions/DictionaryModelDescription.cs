75d1439894ac293fda993343b9cae6a2b9d9fc16
Netanel Aknin <netanelos152@gmail.com>
2021-06-01 08:55:07 +0300

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JokesPrj.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JokesPrj.WebApiApplication" Language="C#" %>
