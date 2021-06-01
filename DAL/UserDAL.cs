d5276fa00e2413bdd16ecb1413791a731d092d23
Netanel Aknin <netanelos152@gmail.com>
2021-06-01 14:46:27 +0300

    fix encrypt

--- DAL/UserDAL.cs
+++ DAL/UserDAL.cs
@@ -14,7 +14,7 @@ namespace JokesPrj.DAL
         }
 
         /*Get user from database*/
-        public User GetUser(string username, string pass)
+        public User GetUserSalt(string username, string pass)
         {
             try
             {
@@ -25,13 +25,39 @@ namespace JokesPrj.DAL
                     string query = $"SELECT * FROM JokesUsers where username = @username";
                     SqlCommand cmd = new SqlCommand(query, con);
                     cmd.Parameters.AddWithValue("@username", username);
-                    cmd.Parameters.AddWithValue("@pass", pass);
                     SqlDataReader reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-                        //u = new User(Convert.ToString(reader["personal_id"]), Convert.ToString(reader["email"]));
+
+                        u = new User(Convert.ToString(reader["salt"]));
                     }
-                    Globals.Encryption.EncodePassword(u, pass);
+                    string salted_hash = Globals.Encryption.EncodePassword(u, pass);
+                    VerifyUser(u, salted_hash);
+                    return u;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public User VerifyUser(User u, string salted_hash)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    con.Open();
+                    string query = $"SELECT * FROM JokesUsers where username = @username AND salted_hash = @salted_hash ";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@username", u.Username);
+                    cmd.Parameters.AddWithValue("@salted_hash", salted_hash);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+
+                        u = new User(Convert.ToInt32(reader["id_user"]), Convert.ToString(reader["username"]), Convert.ToString(reader["email"]), Convert.ToString(reader["user_img"]));
+                    }
                     return u;
                 }
             }
--- Models/User.cs
+++ Models/User.cs
@@ -9,7 +9,7 @@ namespace JokesPrj.Models
     {
         int id_user;
         string username;
-        string salted_hash;
+        //string salted_hash;
         string salt;
         string email;
         string user_img;
@@ -18,16 +18,23 @@ namespace JokesPrj.Models
         {
 
         }
+        public User(int id_user, string username, string email, string user_img)
+        {
+            Id_user = id_user;
+            Username = username;
+            Email = email;
+            User_img = user_img;
+        }
+
+
         public User(string username)
         {
             Username = username;
         }
 
 
-
         public int Id_user { get => id_user; set => id_user = value; }
         public string Username { get => username; set => username = value; }
-        public string Salted_hash { get => salted_hash; set => salted_hash = value; }
         public string Salt { get => salt; set => salt = value; }
         public string Email { get => email; set => email = value; }
         public string User_img { get => user_img; set => user_img = value; }
