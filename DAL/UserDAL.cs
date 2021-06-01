14bb9de7107b746ea2e72b2f04ceb4de6a18c019
Netanel Aknin <netanelos152@gmail.com>
2021-06-01 09:07:00 +0300

    first upload

---
+++ DAL/UserDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JokesPrj.DAL
+{
+    public class UserDAL
+    {
+        private readonly string conStr;
+        public UserDAL(string conStr)
+        {
+            this.conStr = conStr;
+        }
+    }
+}
---
+++ Globals.cs
@@ -0,0 +1,34 @@
+﻿using JokesPrj.Controllers;
+using System.Configuration;
+using JokesPrj.DAL;
+using JokesPrj.Models;
+
+namespace JokesPrj
+{
+    public static class Globals
+    {
+        //initializing data access layer with sql server before start the work between client side and backnd side.
+        #region ctor
+        static Globals()
+        {
+            //get connection string from Web.config;
+            var conStr = ConfigurationManager.ConnectionStrings["LocalDB"].ConnectionString;
+            //var conStr = ConfigurationManager.ConnectionStrings["LIVEDNSfromLocal"].ConnectionString;
+            //var conStr = ConfigurationManager.ConnectionStrings["LIVEDNSfromLivedns"].ConnectionString;
+            Globals.UserDAL = new UserDAL(conStr);
+        }
+        #endregion
+
+        #region Controllers
+        public static UserController UserController { get; set; }
+        #endregion
+
+        #region DAL
+        public static UserDAL UserDAL { get; set; }
+        #endregion
+
+        #region Models
+        public static User User { get; set; }
+        #endregion
+    }
+}
