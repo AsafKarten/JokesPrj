5894a54a9ae1f331c319bb00845494ff7ccc597e
Netanel Aknin <netanelos152@gmail.com>
2021-07-31 15:29:08 +0300

    remove unsed style on App.js

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import StackNavigation from './Components/StackNavigation'
-import { StyleSheet } from 'react-native';
 
 export default function App() {
-
   return (
     <StackNavigation />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
